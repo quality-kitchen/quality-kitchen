71d9c876efba19e38f1d9c2f184484bfa50fb89e
cosmo3769 <piyushthakur3769@gmail.com>
2022-09-11 19:00:22 +0530

    added different logo

--- src/components/comingsoon/comingsoon.jsx
+++ src/components/comingsoon/comingsoon.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import logo from '../../images/logo.png';
+// import logo from '../../images/logo.png';
+import logowhite from '../../images/logo-whitebg.png'
 import './style.css'
 
 export default function ComingSoon() {
     return (
         <Container>
             <Row>
-                <img className="logo-image" src={logo} alt="logo" />
+                {/* <img className="logo-image" src={logo} alt="logo" /> */}
+                <img className="logo-image" src={logowhite} alt="logo" />
             </Row>
             <Row>
                 <h1>Coming Soon ...</h1>
