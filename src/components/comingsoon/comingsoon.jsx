b9e70938c73d8f2156111f235e0f7b6d7d824a59
Ayush <ayushmishra2854@gmail.com>
2022-10-09 15:13:12 +0530

    initialized tailwind and removed bootstrap

--- src/components/comingsoon/comingsoon.jsx
+++ src/components/comingsoon/comingsoon.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
 // import logo from '../../images/logo.png';
 import logowhite from '../../images/logo-whitebg.png'
 import './style.css'
 
 export default function ComingSoon() {
     return (
-        <Container>
-            <Row>
-                {/* <img className="logo-image" src={logo} alt="logo" /> */}
+        <div className="text-3xl font-bold ">
                 <img className="logo-image" src={logowhite} alt="logo" />
-            </Row>
-            <Row>
-                <h1>Coming Soon ...</h1>
-            </Row>
-        </Container>
+                <h1 className="center">Coming Soon ...</h1>
+        </div>
+            
+        
     )
 }
